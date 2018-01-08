330271c34431a73ac470d3148589bce8cbeddd8e
Oliver Backes <olb413@student.hks.harvard.edu>
2018-01-08 17:04:48 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+# This portion of the code defines the multiply function 
+def multiply(a,b):
+	return a * b 
+
+# This portion of the code defines the add function
+def add(a,b):
+	return a + b
+
+#This portion of the code defines the subtract function
+def subtract(a,b):
+	return a - b
+
+#This portion of the code defines the divide function
+def divide(a,b):
+	return a / b
