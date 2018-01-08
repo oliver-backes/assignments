fb022a987bc94083ec3e070022de11299f4f2690
Oliver Backes <olb413@student.hks.harvard.edu>
2018-01-08 17:47:14 -0500

    add call of multiply function and explanation

--- calculator.js
+++ calculator.js
@@ -17,3 +17,8 @@ function subtract(a,b) {
 function divide (a,b) {
 	return (a / b)
 }
+
+//This portion of the code calls the multiply function and provides an explanation
+console.log("I'm going to use the calculator to multiply 2 and 36")
+var x = multiply(2,36)
+console.log(x)
