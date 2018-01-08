8c6311e16c8466fa2c4a95de74bcfc21d258b1f2
Oliver Backes <olb413@student.hks.harvard.edu>
2018-01-08 17:44:45 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,19 @@
+//This portion of the code defines the multiply function
+function multiply(a,b) {
+	return (a * b)
+}
+
+//This portion of the code defines the add function
+function add(a,b) {
+	return (a + b)
+}
+
+//This portion of the code defines the subtract function
+function subtract(a,b) {
+	return (a - b)
+}
+
+//This portion of the code defines the divide function
+function divide (a,b) {
+	return (a / b)
+}
