ab2db71482d113a2653c57a18ceed2a3b88445e6
Oliver Backes <olb413@student.hks.harvard.edu>
2018-01-08 17:08:09 -0500

    add call of multiply function and explanation

--- calculator.py
+++ calculator.py
@@ -15,3 +15,8 @@ def subtract(a,b):
 #This portion of the code defines the divide function
 def divide(a,b):
 	return a / b
+
+#This portion of the code calls the multiply function and provides an explanation
+print "I'm going to use the calculator function to multiply 2 and 36:"
+x = multiply(2,36)
+print x
