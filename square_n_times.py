a84e93de9bf303eed546dc44bd332c834aa15c4a
Oliver Backes <olb413@student.hks.harvard.edu>
2018-01-09 14:35:03 -0500

    add functioning input check

--- square_n_times.py
+++ square_n_times.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python2
+import sys
 
-base = input('Enter your base: ')
+inputs = []
 
-exponent = input('Enter your exponent: ')
+base = raw_input('Enter your base: ')
+
+inputs.append(base)
+
+exponent = raw_input('Enter your exponent: ')
+
+inputs.append(exponent)
+
+print inputs
+
+for input in inputs:
+	if not (input.isdigit()):
+		sys.exit(input + " is not a number.")
 
 #This portion of the code defines the square_n_times functon
 def square_n_times(number,n):
